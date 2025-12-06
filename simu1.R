7aa4fa759782afb8bee295e0eac0b4bc2ecd136e
zheng017 <58981608+zheng017@users.noreply.github.com>
2025-12-06 20:31:54 +0800

    Update simu1.R

--- simu1.R
+++ simu1.R
@@ -8,30 +8,25 @@ gc()
 sourceCpp("utils1.cpp")
 sourceCpp("MCUB.cpp")
 
-n=200
-p=10
-d=2
+n = 400
+p = 20
+d = 2
 q = 0
 m = 5
-b0 = 1
 max_r = ifelse(d < 5, 5, 8)
 pai_param = c(.7, 1)
-type1 = "uniform"
-type2 = "identity"
 
-#set.seed(seed)
-#data = generate_data(n, p, q, d, m)
-data = generate_data_FACUB(n, p, q, d, m, b0, pai_param, type1, type2)
+data = generate_data_FACUB(n, p, q, d, m, pai_param)
 R = data$R
 x = data$x
 
-initials1 = generate_initials(R, x, d, type = "3", 0, 0, 0.1, 0.5)
+initials1 = generate_initials(R, x, d, B0 = 0, beta0 = 0, sigma0 = 0.1, pai0 = 0.5)
 
 res1 = FAVA_cpp(R, x, m, d, initials1)
 res2 = MCUB_cpp(R, x, m, b0 = rep(1, p))
 res3 = MCUB_cpp(R, cbind(x, data$f), m, b0 = rep(1, p))
 
-hic1 = hic_new(R, x, m, max_r)
+hic1 = hic(R, x, m, max_r)
 
 evaluate_FACUB(data, res1)
 evaluate_MCUB(data, res2)
